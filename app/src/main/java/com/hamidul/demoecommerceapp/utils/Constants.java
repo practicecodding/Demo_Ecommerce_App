4ccd3b5f1c9d0ad9d36d3aa079ebc91eed82be25
S.M.Hamidul <smhamidul6195@gmail.com>
2024-09-24 20:45:28 +0600

    Demo Ecommerce App

---
+++ app/src/main/java/com/hamidul/demoecommerceapp/utils/Constants.java
@@ -0,0 +1,8 @@
+package com.hamidul.demoecommerceapp.utils;
+
+public class Constants {
+
+    public static String API_BASE_URL = "";
+    public static String GET_CATEGORIES_URL = API_BASE_URL + "";
+
+}
