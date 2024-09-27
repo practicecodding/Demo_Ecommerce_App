1d4775d685e1d6de18be797f91b8fb518d199f96
S.M.Hamidul <smhamidul6195@gmail.com>
2024-09-27 08:52:20 +0600

    Demo Ecommerce App

--- Web Source Code/view_product.php
+++ Web Source Code/view_product.php
@@ -12,13 +12,13 @@ $data = array();
 
 foreach($result as $item){
 	
-	$products['id'] = $item['id'];
-	$products['id'] = $item['id'];
-	$products['id'] = $item['id'];
-	$products['id'] = $item['id'];
-	$products['id'] = $item['id'];
-	$products['id'] = $item['id'];
-	$products['id'] = $item['id'];
+	$products['id'] = (int) $item['id'];
+	$products['name'] = $item['name'];
+	$products['image_url'] = $item['image_url'];
+	$products['status'] = $item['status'];
+	$products['price'] =(double)$item['price'];
+	$products['discount'] = (double)$item['discount'];
+	$products['stock'] = (int)$item['stock'];
 	array_push($data,$products);
 	
 }
