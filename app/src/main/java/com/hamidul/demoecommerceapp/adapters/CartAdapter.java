33c27cbd8a2eefa8ac58a12bde5d87728113fcc4
S.M.Hamidul <smhamidul6195@gmail.com>
2024-09-29 07:31:08 +0600

    Demo Ecommerce App

--- app/src/main/java/com/hamidul/demoecommerceapp/adapters/CartAdapter.java
+++ app/src/main/java/com/hamidul/demoecommerceapp/adapters/CartAdapter.java
@@ -1,16 +1,20 @@
 package com.hamidul.demoecommerceapp.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.hamidul.demoecommerceapp.R;
 import com.hamidul.demoecommerceapp.databinding.ItemCartBinding;
+import com.hamidul.demoecommerceapp.databinding.QuantityDialogBinding;
 import com.hamidul.demoecommerceapp.model.Product;
 
 import java.util.ArrayList;
@@ -39,6 +43,49 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.cartViewHolder
                 .into(holder.binding.image);
         holder.binding.name.setText(product.getName());
         holder.binding.price.setText("TK " + product.getPrice());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                QuantityDialogBinding quantityDialogBinding = QuantityDialogBinding.inflate(LayoutInflater.from(context));
+
+                AlertDialog dialog = new AlertDialog.Builder(context)
+                        .setView(quantityDialogBinding.getRoot())
+                        .create();
+
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+                quantityDialogBinding.productName.setText(product.getName());
+                quantityDialogBinding.productStock.setText("Stock : "+product.getStock());
+
+                int stock = product.getStock();
+
+                quantityDialogBinding.negativeBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                });
+
+                quantityDialogBinding.positiveBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                });
+
+                quantityDialogBinding.saveBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                });
+
+                dialog.show();
+
+            }
+        });
 
     }
 
